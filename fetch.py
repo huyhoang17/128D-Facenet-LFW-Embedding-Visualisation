f2a9ca06aefbf9eb25a254b128b20aed05f8e11e
huyhoang17 <hoangphan0710@gmail.com>
2018-01-17 16:29:51 +0700

    update config file and add LFW labels dataset

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 import json
 
@@ -12,14 +13,21 @@ EMBEDDING_MODEL = "models/128D_LFW.pkl"
 TENSOR_NAME = "LFW"  # Label Faces in the Wild
 CONFIG_PATH = os.path.join(BASE_DIR, "oss_demo_projector_config.json")
 TENSOR_PATH = os.path.join(BASE_DIR, "tensor.bytes")
+LABELS_PATH = os.path.join(BASE_DIR, "LFW_labels.tsv")
 IMAGE_SPRITES_PATH = os.path.join(BASE_DIR, "sprites.png")
 IMAGE_SIZE = 28
 
 
 def main():
 
-    labels, _, emb_arrays = joblib.load(EMBEDDING_MODEL)
+    labels, class_names, emb_arrays = joblib.load(EMBEDDING_MODEL)
     emb_arrays.astype(np.float32).tofile(TENSOR_PATH)
+
+    with open(LABELS_PATH, 'w') as f:
+        c = Counter(labels)
+        for i in c:
+            for _ in range(c[i]):
+                f.write(class_names[i] + "\n")
 
     with open(CONFIG_PATH) as f:
         oss_json = json.load(f)
@@ -28,6 +36,7 @@ def main():
         "tensorName": TENSOR_NAME,
         "tensorShape": [len(labels), emb_arrays.shape[1]],
         "tensorPath": TENSOR_PATH,
+        "metadataPath": LABELS_PATH,
         "sprite": {
             "imagePath": IMAGE_SPRITES_PATH,
             "singleImageDim": [IMAGE_SIZE, IMAGE_SIZE]
